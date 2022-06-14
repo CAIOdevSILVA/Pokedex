623258273188e5e8ceb2076ea82cb81171bff93f
Caio Silva <caiossantosp5563@gmail.com>
2022-06-14 18:11:55 -0300

    terceiro commit

--- src/index.js
+++ src/index.js
@@ -1,34 +1,41 @@
 async function fetchPokemon(){
-  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${12}`);
-  const result = await response.json();
-  console.log(result)
-  
-  const numberOfTypes = () => {
+  pokemons = []
+  for(let i=1; i<=150; i++){
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
+    const result = await response.json();
+    pokemons.push(result);
+  }
+  console.log(pokemons)
+
+  /* const numberOfTypes = () => {
     arrayOfTypes = [];
     result.types.forEach((element) => {
       arrayOfTypes.push(element.type.name)
     } )
     return arrayOfTypes.length === 1 ? `${arrayOfTypes[0]}`: `${arrayOfTypes[0]} | ${arrayOfTypes[1]}`;
   }
-  
-  const card = `
-  <h2 class="title">Pokedex</h2>
-  <div class="card">
-    <div class="image"><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${12}.png" alt=""></div>
-    <h2 class="pokemonName">${result.name}</h2>
-    <div class="stats">
-      <div class="stats-content">
-        <span>Type: ${numberOfTypes()}</span>
-        <span>${result.stats[1].stat.name}: ${result.stats[1].base_stat}</span>
-        <span>${result.stats[2].stat.name}: ${result.stats[2].base_stat}</span>
-        <span>${result.stats[3].stat.name}: ${result.stats[3].base_stat}</span>
-        <span>${result.stats[4].stat.name}: ${result.stats[4].base_stat}</span>
+   */
+  pokemons.forEach((element, index) => {
+    const card = `
+    <div class="card">
+      <div class="image"><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index+1}.png" alt=""></div>
+      <h2 class="pokemonName">${element.name}</h2>
+      <div class="stats">
+        <div class="stats-content">
+          <span>Type:  </span>
+          <span>${element.stats[1].stat.name}: ${element.stats[1].base_stat}</span>
+          <span>${element.stats[2].stat.name}: ${element.stats[2].base_stat}</span>
+          <span>${element.stats[3].stat.name}: ${element.stats[3].base_stat}</span>
+          <span>${element.stats[4].stat.name}: ${element.stats[4].base_stat}</span>
+        </div>
       </div>
     </div>
-  </div>
-  `
-  const container = document.querySelector(".container");
-  container.innerHTML = card
+    `
+    const container = document.querySelector(".container");
+    const cardElements = document.createElement("div");
+    cardElements.innerHTML = card
+    container.appendChild(cardElements);
+  })
 }
 
 fetchPokemon()
