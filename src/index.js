bf979efd5b2ea4083d683c7d115bd788bd5286c5
Caio Silva <caiossantosp5563@gmail.com>
2022-06-14 17:19:37 -0300

    adição da função para identificar numero de tipos

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 async function fetchPokemon(){
-  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${111}`);
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${12}`);
   const result = await response.json();
-  //console.log(result)
+  console.log(result)
+  
+  const numberOfTypes = () => {
+    arrayOfTypes = [];
+    result.types.forEach((element) => {
+      arrayOfTypes.push(element.type.name)
+    } )
+    return arrayOfTypes.length === 1 ? `${arrayOfTypes[0]}`: `${arrayOfTypes[0]} | ${arrayOfTypes[1]}`;
+  }
+  
   const card = `
-  <h2 class="title">PokeCard</h2>
+  <h2 class="title">Pokedex</h2>
   <div class="card">
-    <div class="image"><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${111}.png" alt=""></div>
+    <div class="image"><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${12}.png" alt=""></div>
     <h2 class="pokemonName">${result.name}</h2>
     <div class="stats">
       <div class="stats-content">
-        <span>Type: <span class="grass">${result.types[0].type.name}</span> <span class="poison">${result.types[1].type.name}</span></span>
+        <span>Type: ${numberOfTypes()}</span>
         <span>${result.stats[1].stat.name}: ${result.stats[1].base_stat}</span>
         <span>${result.stats[2].stat.name}: ${result.stats[2].base_stat}</span>
         <span>${result.stats[3].stat.name}: ${result.stats[3].base_stat}</span>
@@ -23,3 +32,4 @@ async function fetchPokemon(){
 }
 
 fetchPokemon()
+
